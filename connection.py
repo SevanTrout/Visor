4558fb90bf3a81ed4989b6c5d26a03c80aec18cf
Artyom Sidyakin <sidyackin.artyom@gmail.com>
2019-05-15 00:08:38 +0300

    Drop unused table;

--- connection.py
+++ connection.py
@@ -23,30 +23,4 @@ def create_connection():
     query.exec_("insert into person values(104, 'Roberto', 'Robitaille')")
     query.exec_("insert into person values(105, 'Maria', 'Papadopoulos')")
 
-    query.exec_("""CREATE TABLE IF NOT EXISTS  items (id INT primary key, 
-                                       imagefile INT,
-                                       itemtype varchar(20),
-                                       description varchar(100))""");
-    query.exec_("insert into items "
-                "values(0, 0, 'Qt',"
-                "'Qt is a full development framework with tools designed to "
-                "streamline the creation of stunning applications and  "
-                "amazing user interfaces for desktop, embedded and mobile "
-                "platforms.')");
-    query.exec_("insert into items "
-                "values(1, 1, 'Qt Quick',"
-                "'Qt Quick is a collection of techniques designed to help "
-                "developers create intuitive, modern-looking, and fluid "
-                "user interfaces using a CSS & JavaScript like language.')");
-    query.exec_("insert into items "
-                "values(2, 2, 'Qt Creator',"
-                "'Qt Creator is a powerful cross-platform integrated "
-                "development environment (IDE), including UI design tools "
-                "and on-device debugging.')");
-    query.exec_("insert into items "
-                "values(3, 3, 'Qt Project',"
-                "'The Qt Project governs the open source development of Qt, "
-                "allowing anyone wanting to contribute to join the effort "
-                "through a meritocratic structure of approvers and "
-                "maintainers.')");
     return True
